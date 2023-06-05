b82efa3880edf1658f06f85442aaa5ad5ea3755f
AlexBruma0 <105242355+AlexBruma0@users.noreply.github.com>
2023-06-04 17:29:07 -0700

    Initial Commit

---
+++ Assign2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Assign2
+//
+//  Created by Alex  Bruma on 2023-06-04.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
